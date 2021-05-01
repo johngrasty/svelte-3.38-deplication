980435270969992d147329990fba755d0f979013
John Grasty <johnpgrasty@gmail.com>
2021-05-01 13:58:49 +0200

    * Should be minimal example of the problem.

---
+++ src/lib/Header.svelte
@@ -0,0 +1,3 @@
+<header>
+	<h1>Testing</h1>
+</header>
---
+++ src/routes/test/test.svelte
@@ -0,0 +1,8 @@
+<svelte:head>
+	<title>Title</title>
+</svelte:head>
+
+<h3>Contact</h3>
+<p>Testing sibling elements</p>
+
+{@html '<p>Testing &#64;html </p>'}
