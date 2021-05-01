21df13b36e7e9951254caed0bb6f14f4dabe8d44
John Grasty <johnpgrasty@gmail.com>
2021-05-01 14:11:46 +0200

    * Added link

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,3 +4,4 @@
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p><a href="/test">Bug (though it may also be visible on this homepage.</a></p>
